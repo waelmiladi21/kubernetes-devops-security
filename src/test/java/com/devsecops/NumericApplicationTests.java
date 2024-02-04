f1005ed3b8c4a1264182a19ca3e147acd80c52e6
wael <wael.miladi@eniso.u-sousse.tn>
2024-02-04 23:24:54 +0100

    modify tests1

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -52,11 +52,7 @@ public class NumericApplicationTests {
                 .andExpect(content().string("Kubernetes DevSecOps"));
     }
 
-    @Test
-    public void incrementByOneMessage() throws Exception {
-         this.mockMvc.perform(get("/increment/50")).andDo(print()).andExpect(status().isOk())
-           .andExpect(content().string("51"));
-    }
+    
     
 
 }
