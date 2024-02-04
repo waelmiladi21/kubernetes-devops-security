d8f4fcd15e0fc08194891f92023adc520eb7015f
wael <wael.miladi@eniso.u-sousse.tn>
2024-02-04 22:57:57 +0100

    added mutation plugin and stage

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -50,6 +50,12 @@ public class NumericApplicationTests {
     public void welcomeMessage() throws Exception {
          this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
     }
+
+    @Test
+    public void incrementByOneMessage() throws Exception {
+         this.mockMvc.perform(get("/increment/50")).andDo(print()).andExpect(status().isOk())
+           .andExpect(content().string("51"));
+    }
     
 
 }
