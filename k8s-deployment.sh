f4395e1ee10ef90cec5dfe4d2e20be542a88797d
wael <wael.miladi@eniso.u-sousse.tn>
2024-02-10 23:25:27 +0100

    addedd volume21

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+ sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 # kubectl -n default get deployment ${deploymentName} > /dev/null
 
 # if [[ $? -ne 0 ]]; then
